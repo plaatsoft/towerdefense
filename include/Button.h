c9186a16b3c950690f8e442ae06e3c50e38c841f
wplaat <wplaat@6ba80b68-d6d0-11de-a840-e5196caed149>
2009-12-01 06:30:17 +0000

    Next development phase

--- include/Button.h
+++ include/Button.h
@@ -32,23 +32,21 @@ class Button
 	GRRLIB_texImg *imageFocus;
 	int height;
 	int width;
-	int rumble;
 	char label[100];
 	
   public:
 	// Constructor & Destructor
   	Button();
-	Button(int x,int y, GRRLIB_texImg *imageNormal, GRRLIB_texImg *imageFocus );
  	~Button();
 
 	// Other
-	void draw(int pointerX, int pointerY);
+	void draw();
 	boolean onSelect(int pointerX, int pointerY);
 	
 	// Setters
-	void setLabel(const char *label);
 	void setX(int x);
 	void setY(int y);
+	void setLabel(const char *label);
 	void setImageNormal(GRRLIB_texImg *imageNormal);
 	void setImageFocus(GRRLIB_texImg *imageFocus);
 };
