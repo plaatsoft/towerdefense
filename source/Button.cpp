6bd2aae9369d8326f888f256e8a2f13e025671f0
wplaat <wplaat@6ba80b68-d6d0-11de-a840-e5196caed149>
2010-01-29 06:33:55 +0000

    Improve source code documentation

--- source/Button.cpp
+++ source/Button.cpp
@@ -1,23 +1,26 @@
-/*  
-**  Created by wplaat (www.plaatsoft.nl)
-**
-**  Copyright (C) 2009-2010
-**  =======================
-**
-**  This program is free software; you can redistribute it and/or modify
-**  it under the terms of the GNU General Public License as published by
-**  the Free Software Foundation, version 2.
-**
-**  This program is distributed in the hope that it will be useful,
-**  but WITHOUT ANY WARRANTY; without even the implied warranty of
-**  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-**  GNU General Public License for more details.
-**
-**  You should have received a copy of the GNU General Public License
-**  along with this program; if not, write to the Free Software
-**  Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
-*/
-
+/** 
+ *  @file 
+ *  @brief The file contains the Button class
+ *
+ *  Created by wplaat (www.plaatsoft.nl)
+ *
+ *  Copyright (C) 2009-2010
+ *  =======================
+ *
+ *  This program is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, version 2.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program; if not, write to the Free Software
+ *  Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ */ 
+ 
 #include <wiiuse/wpad.h>
 
 #include "General.h"
